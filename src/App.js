3903c960136eb8e1596f4412316f713c36b05750
eastcopper <ldh7228@gmail.com>
2022-03-12 13:30:01 +0900

    dswacxwds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Dobby from "./Components/Page/Dobby";
+import GlobalStyle from "./Style/styles";
 
 function App() {
   return (
     <>
       <Dobby />
+      <GlobalStyle />
     </>
   );
 }
---
+++ src/Components/Page/Dobby/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 200px;
+`;
+
+export const Title = styled.div`
+  color: #57a0ce;
+  font-size: 120px;
+  font-weight: bold;
+`;
+
